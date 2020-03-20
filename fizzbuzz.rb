fec9a033c0c66898fb92cf18ce398e93722d4a3e
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-20 18:24:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  if
-    3%3 == 0 || 5%5 == 0
-   "fizzbuzz"
-  elsif
-    3%3 == 0
-    "Fizz"
-  else
-    5%5 == 0
-    "Buzz"
-  end
-end
+
